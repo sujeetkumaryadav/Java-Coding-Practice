f8e918c458c2c57c05c970645c1e5fd7f0012aed
sujeetcse <sujeetkumaryadavs791@gmail.com>
2026-05-26 10:54:47 +0530

    Largest of three num check

---
+++ LargestOfThreeNum.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+ class LargestOfThreeNum {
+    public static void main(String[] args) {
+      
+      Scanner sc=new Scanner(System.in);
+      int n1=sc.nextInt();
+       int n2=sc.nextInt();
+        int n3=sc.nextInt();
+    
+      if(n1>n2 && n1>n3)
+      System.out.println("Largest Number is "+n1);
+      else if(n2>n1 && n2>n3)
+      System.out.println("Largest Number is "+n2);
+      else
+      System.out.println("Largest Number is "+n3);
+    }
+}
