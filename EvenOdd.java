7a2ce8ffeb2375ed3181403b4366eb6201751499
sujeetcse <sujeetkumaryadavs791@gmail.com>
2026-05-26 10:23:43 +0530

    Even Odd check

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+ class EvenOdd {
+    public static void main(String[] args) {
+      
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      if(n%2==0)
+      System.out.println("Even Number");
+      else
+      System.out.println("Odd Number");
+    }
+}
