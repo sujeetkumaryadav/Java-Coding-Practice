b21e45ac51894c7a751880fd39ecfc17060f499d
sujeetcse <sujeetkumaryadavs791@gmail.com>
2026-05-26 11:04:25 +0530

    Number Sign check

---
+++ NumbSignCheck.java
@@ -0,0 +1,16 @@
+ import java.util.Scanner;
+ class NumbSignCheck {
+    public static void main(String[] args) {
+      
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      
+    
+      if(n>0)
+      System.out.println("Number is Positive ");
+      else if(n<0)
+      System.out.println("Number is Negative");
+      else
+      System.out.println("Number is Zero ");
+    }
+}
