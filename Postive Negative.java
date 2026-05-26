55a28f95255da4779ffba938313f12782ccd4742
sujeetcse <sujeetkumaryadavs791@gmail.com>
2026-05-26 10:18:08 +0530

    Postive Negative check

---
+++ Postive Negative.java
@@ -0,0 +1,14 @@
+ import java.util.Scanner;
+ class Demo {
+    public static void main(String[] args) {
+      
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      if(n>0)
+      System.out.println("Positive Number");
+      else if(n<0)
+      System.out.println("Negative Number");
+      else
+      System.out.println("Number is Zero");
+    }
+}
